588c20fd5f979b602a926a1f4f752957af914cb8
will <armstrow@umd.edu>
2014-12-02 16:16:03 -0500

    Added missing XML file

--- StoryBuddies/gen/courses/cmsc436/storybuddies/R.java
+++ StoryBuddies/gen/courses/cmsc436/storybuddies/R.java
@@ -9,7 +9,7 @@ package courses.cmsc436.storybuddies;
 
 public final class R {
     public static final class array {
-        public static final int stories=0x7f080000;
+        public static final int stories=0x7f070000;
     }
     public static final class attr {
     }
@@ -21,17 +21,17 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f060000;
-        public static final int activity_vertical_margin=0x7f060001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int start_button=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f0b0002;
-        public static final int footerView=0x7f0b0001;
-        public static final int imageButton1=0x7f0b0000;
+        public static final int action_settings=0x7f0a0002;
+        public static final int footerView=0x7f0a0001;
+        public static final int imageButton1=0x7f0a0000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -39,15 +39,12 @@ public final class R {
         public static final int story_list_item=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f0a0000;
-    }
-    public static final class raw {
-        public static final int bubble_pop=0x7f050000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f070002;
-        public static final int add_story=0x7f070001;
-        public static final int app_name=0x7f070000;
+        public static final int action_settings=0x7f060002;
+        public static final int add_story=0x7f060001;
+        public static final int app_name=0x7f060000;
     }
     public static final class style {
         /** 
@@ -71,10 +68,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f090000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
          */
-        public static final int AppTheme=0x7f090001;
+        public static final int AppTheme=0x7f080001;
     }
     public static final class xml {
         public static final int nfc_tech_filter=0x7f040000;
