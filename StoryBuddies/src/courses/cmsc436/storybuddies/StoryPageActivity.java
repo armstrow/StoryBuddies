58a059e9d3572544653befa6d19652883c43a3b4
Sara <Sara@10.108.205.127>
2014-12-06 00:36:58 -0500

    Added story to strings and page turning with gestures

--- StoryBuddies/src/courses/cmsc436/storybuddies/StoryPageActivity.java
+++ StoryBuddies/src/courses/cmsc436/storybuddies/StoryPageActivity.java
@@ -1,25 +1,43 @@
 package courses.cmsc436.storybuddies;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.Prediction;
+import android.gesture.GestureOverlayView.OnGesturePerformedListener;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class StoryPageActivity extends Activity{
+public class StoryPageActivity extends Activity implements OnGesturePerformedListener{
+	
+	
 	private final String TAG = "StoryPage";
 	private int currPage = 0;
 	private SpeechEngine speech;
 	private EditText storyText;
 	
-	private StoryBook currStory;
+	private int currStoryPos;
 	
+	private StoryBook currStory;
+
+	private GestureLibrary mLibrary;
+	
+	private GestureDetector mGestureDetector;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,7 +45,7 @@ public class StoryPageActivity extends Activity{
 		storyText = (EditText) findViewById(R.id.text);
 		speech = SpeechEngine.getInstance(getApplicationContext());
 		
-		final int currStoryPos = getIntent().getIntExtra("position",0);
+		currStoryPos = getIntent().getIntExtra("position",0);
 		currStory = StartScreenActivity.stories.get(currStoryPos);
 		
 		updatePage();
@@ -35,19 +53,16 @@ public class StoryPageActivity extends Activity{
 		ImageButton prevButton = (ImageButton) findViewById(R.id.lastPage);
 		ImageButton nextButton = (ImageButton) findViewById(R.id.nextPage);
 		
+
+		mLibrary = GestureLibraries.fromRawResource(this, R.raw.gestures);
+		GestureOverlayView gestureOverlay = (GestureOverlayView) findViewById(R.id.gestureOverlay);
+		gestureOverlay.addOnGesturePerformedListener(this);
+		
 		prevButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				prevPage();
 				Log.i(TAG,"Entered prevButton OnClickListener");
-				if(currPage > 0){
-					currPage--;
-					updatePage();
-				} else {
-					Intent goToCoverIntent = new Intent(StoryPageActivity.this,CoverPageActivity.class);
-					goToCoverIntent.putExtra("position", currStoryPos);
-					startActivity(goToCoverIntent);
-					finish();
-				}
 				
 			}
 		});
@@ -55,15 +70,25 @@ public class StoryPageActivity extends Activity{
 		nextButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				nextPage();
 				Log.i(TAG,"Entered nextButton OnClickListener");
-				if(currPage < currStory.getmPages().size()-1){
-					currPage++;
-					updatePage();	
-				} else {
-					finish();
-				}			
+							
 			}
 		});
+
+		gestureOverlay.setOnTouchListener( new OnTouchListener() {
+		
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				Log.i(TAG, "Entered startButton.OnTouchListener.onTouch()");
+				return mGestureDetector.onTouchEvent(event);
+
+			}
+		});
+		
+		if (!mLibrary.load()) {
+			finish();
+		}
 	}
 	
 	private void updatePage(){
@@ -90,5 +115,45 @@ public class StoryPageActivity extends Activity{
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+		// TODO Auto-generated method stub
+		ArrayList<Prediction> predictions = mLibrary.recognize(gesture);
+
+		if (predictions.size() > 0) {
+
+			// Get highest-ranked prediction
+			Prediction prediction = predictions.get(0);
+
+			if (prediction.name.equals("nextPage")) {
+				nextPage();
+			} else if (prediction.name.equals("prevPage")) {
+				prevPage();
+			}
+		
+		}
+	}
+	
+	private void nextPage() {
+		if(currPage < currStory.getmPages().size()-1){
+			currPage++;
+			updatePage();	
+		} else {
+			finish();
+		}
+	}
+	
+	private void prevPage() {
+		if(currPage > 0){
+			currPage--;
+			updatePage();
+		} else {
+			Intent goToCoverIntent = new Intent(StoryPageActivity.this,CoverPageActivity.class);
+			goToCoverIntent.putExtra("position", currStoryPos);
+			startActivity(goToCoverIntent);
+			finish();
+		}
 	}
 }
