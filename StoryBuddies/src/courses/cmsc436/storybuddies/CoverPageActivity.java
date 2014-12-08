9c8d27d65f9250a4445789571ee62243a932a120
Sara <Sara@10.108.205.127>
2014-12-08 17:41:01 -0500

    added cover image

--- StoryBuddies/src/courses/cmsc436/storybuddies/CoverPageActivity.java
+++ StoryBuddies/src/courses/cmsc436/storybuddies/CoverPageActivity.java
@@ -49,6 +49,9 @@ public class CoverPageActivity extends Activity implements OnGesturePerformedLis
 		//TODO - set author to appropriate text
 		
 		ImageButton coverView = (ImageButton) findViewById(R.id.bookCover);
+
+		currStory = StoryBuddiesBaseActivity.stories.get(currStoryPos);
+		coverView.setImageResource(currStory.getmTitlePage());
 		
 		coverView.setOnClickListener(new OnClickListener() {
 			@Override
