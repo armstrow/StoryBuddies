d25031dc7142dc999313c08b6c47602f0424c562
gradyshingler <grady.shingler@gmail.com>
2014-12-12 22:50:34 -0500

    added threeBedGameActivity and its xml file.
    Have not implemented game into story yet.

---
+++ StoryBuddies/src/courses/cmsc436/storybuddies/ThreeBedsGameActivity.java
@@ -0,0 +1,54 @@
+package courses.cmsc436.storybuddies;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+public class ThreeBedsGameActivity extends Activity {
+	private final String TAG = "ThreeBedsGameActivity";
+	private SpeechEngine speech;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.i(TAG, "Entered ThreeBedsGame Activity");
+		super.onCreate(savedInstanceState);
+		speech = SpeechEngine.getInstance(getApplicationContext());
+		setContentView(R.layout.activity_bear_bed_game);
+	
+		ImageView bigBed = (ImageView) findViewById(R.id.bigBed);
+		ImageView medBed = (ImageView) findViewById(R.id.mediumBed);
+		ImageView smallBed = (ImageView) findViewById(R.id.smallBed);
+		
+		bigBed.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.i(TAG,"Entered BigBed OnClickListener");
+				speech.speak("This Bed is to hard!");							
+			}
+		});
+		
+		medBed.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.i(TAG,"Entered medBed OnClickListener");
+				speech.speak("This Bed is to soft!");							
+			}
+		});
+		
+		smallBed.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.i(TAG,"Entered smallBed OnClickListener");
+				speech.speak("This Bed is just right");	
+				Log.i(TAG, "Leaving Game Activity");
+				finish();
+			}
+		});
+	}
+	
+	
+	
+}
