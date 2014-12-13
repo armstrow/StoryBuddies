665ddaf42518286970ac181883d5e3f8b1902d63
gradyshingler <grady.shingler@gmail.com>
2014-12-12 22:07:06 -0500

    Added some basic functionality to the find the rabbit game

--- StoryBuddies/src/courses/cmsc436/storybuddies/GameFindTheRabbitActivity.java
+++ StoryBuddies/src/courses/cmsc436/storybuddies/GameFindTheRabbitActivity.java
@@ -3,18 +3,31 @@ package courses.cmsc436.storybuddies;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class GameFindTheRabbitActivity extends Activity {
 	private final String TAG = "GameFindTheRabbitActivity";
+	private SpeechEngine speech;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.title_page);
-		//TODO - make new xml layout for game activity
 		Log.i(TAG, "Entered Game Activity");
-		Log.i(TAG, "Leaving Game Activity");
-		//TODO - Create Game Mechanics
-		finish();
+		super.onCreate(savedInstanceState);
+		speech = SpeechEngine.getInstance(getApplicationContext());
+		setContentView(R.layout.activity_find_the_rabbit);
+	
+		ImageView hiddenRabbit = (ImageView) findViewById(R.id.hiddenRabbit);
+		
+		hiddenRabbit.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.i(TAG,"Entered hiddenRabbit OnClickListener");
+				speech.speak("Great Job! You have found the sleeping hare!");
+				Log.i(TAG, "Leaving Game Activity");
+				finish();							
+			}
+		});
 	}
 }
